3bb4ab1d95e46e58912e8e995b5f104919fb1eff
Peter Lee <peter.lee@finclab.com>
2022-05-10 15:33:02 +0000

    fixed a start-up error

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- Scripts must be loaded before plugins are loaded
 vim.cmd("source " .. vim.fn.expand("$HOME") .. "/.config/nvim/lua/user/vimscript/init_first.vim")
+
+local config = {
+
+	-- set vim options here (vim.<first_key>.<second_key> =  value)
+	-- options = {
+	-- opt = {
+	-- 	relativenumber = true, -- sets vim.opt.relativenumber
+	-- },
+	-- g = {
+	-- 	mapleader = " ", -- sets vim.g.mapleader
+	-- },
+	-- },
+}
+
+return config
